46d34bb6a1070cdc6633c91e5032736b3207519c
Analkumar02 <analkumar.02.developer@gmail.com>
2025-12-05 19:24:20 +0530

    Initial commit with Next.js project

---
+++ src/slices/Hero/index.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { Children, FC } from "react";
+import { Content } from "@prismicio/client";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { Bounded } from "@/components/Bounded";
+import { Canvas } from "@react-three/fiber";
+import { Scene } from "./Scene";
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero: FC<HeroProps> = ({ slice }) => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+      className="text-shadow/30 blue-gradient-bg relative h-dvh text-white text-shadow-lg"
+    >
+      <div className="hero-scene pointer-events-none sticky top-0 h-dvh w-full">
+        <Canvas shadows="soft">
+          <Scene />
+        </Canvas>
+      </div>
+      <div className="hero-content absolute inset-x-0 top-0 h-dvh w-full">
+        <Bounded
+          fullWidth
+          className="absolute inset-x-0 top-18 md:top-24 md:left-[8vw]"
+        >
+          <PrismicRichText
+            field={slice.primary.heading}
+            components={{
+              heading1: ({ children }) => (
+                <h1 className="hero-heading font-black-slanted text-6xl leading-[0.8] uppercase sm:text-7xl lg:text-8xl">
+                  {children}
+                </h1>
+              ),
+            }}
+          />
+        </Bounded>
+
+        <Bounded
+          fullWidth
+          className="hero-body absolute inset-x-0 bottom-0 md:right-[8vw] md:left-auto"
+          innerClassName="flex flex-col gap-3"
+        >
+          <div className="max-w-md">
+            <PrismicRichText
+              field={slice.primary.body}
+              components={{
+                heading2: ({ children }) => (
+                  <h2 className="font-bold-slanted mb-1 text-4xl uppercase lg:mb-2 lg:text-6xl">
+                    {children}
+                  </h2>
+                ),
+              }}
+            />
+          </div>
+
+          <button className="font-bold-slanted group flex w-fit cursor-pointer items-center gap-1 rounded bg-[#01a7e1] px-3 py-1 text-2xl uppercase transition disabled:grayscale">
+            {slice.primary.buy_button_text}
+            <span className="transition group-hover:translate-x-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16px"
+                height="16px"
+                viewBox="0 0 24 24"
+              >
+                <rect width="16" height="16" fill="none" />
+                <path
+                  fill="#fff"
+                  d="M11.273 3.687a1 1 0 1 1 1.454-1.374l8.5 9a1 1 0 0 1 0 1.374l-8.5 9.001a1 1 0 1 1-1.454-1.373L19.125 12z"
+                  strokeWidth="2"
+                  stroke="#fff"
+                />
+              </svg>
+            </span>
+          </button>
+        </Bounded>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
